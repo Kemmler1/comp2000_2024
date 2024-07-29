8d9dfb5002a22fe3239787f41171d5ac683be88b
Kemmler1 <106467576+Kemmler1@users.noreply.github.com>
2024-07-29 13:18:05 +1000

    W2 Prac

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame{
     public static void main(String[] args) throws Exception {
-        // giving some head room
-        ////////////////////////////////////////////////////
-        System.out.println();
-        System.out.println();
-        ////////////////////////////////////////////////////
-
-        Point3D p3d = new Point3D(5, 7, 9);
-        System.out.println(p3d);
+        Main window = new Main();
     }
 }
